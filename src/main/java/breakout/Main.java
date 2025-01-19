e878d75b313b5ab9cc6226ae6988589f64a3e36d
Calvin Chen <clc162@duke.edu>
2025-01-18 23:31:45 -0500

    Test: remove test Shapes from scene

--- src/main/java/breakout/Main.java
+++ src/main/java/breakout/Main.java
@@ -5,9 +5,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.scene.shape.Circle;
 
 /**
  * @author Robert C. Duvall, Calvin Chen
@@ -19,8 +17,8 @@ public class Main extends Application {
     public static final Color DUKE_BLUE = new Color(0, 0.325, 0.608, 1);
     public static final int SCENE_WIDTH = 1100;
     public static final int SCENE_HEIGHT = 850;
-    public static final int BLOCK_WIDTH = 50;
-    public static final int BLOCK_HEIGHT = 20;
+//    public static final int BLOCK_WIDTH = 50;
+//    public static final int BLOCK_HEIGHT = 20;
 
 
     /**
@@ -28,17 +26,9 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) {
-        Circle shape = new Circle(200, 200, 40);
-        shape.setFill(Color.LIGHTSTEELBLUE);
-
-        Rectangle block = new Rectangle(500, 500, BLOCK_WIDTH, BLOCK_HEIGHT);
-        block.setFill(Color.LIGHTSTEELBLUE);
-
         Paddle paddle = new Paddle(SCENE_WIDTH, SCENE_HEIGHT);
 
         Group root = new Group();
-        root.getChildren().add(shape);
-        root.getChildren().add(block);
         root.getChildren().add(paddle.getShape());
 
         Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT, DUKE_BLUE);

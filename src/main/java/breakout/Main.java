d65914ed686311d77433c21e5f23af39a8955c1f
Calvin Chen <clc162@duke.edu>
2025-01-18 23:24:19 -0500

    Feature BREAK-04: add a controllable paddle  contained by the scene edges

--- src/main/java/breakout/Main.java
+++ src/main/java/breakout/Main.java
@@ -3,39 +3,60 @@ package breakout;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.shape.Circle;
 
-
 /**
- * Feel free to completely change this code or delete it entirely.
- *
- * @author YOUR NAME HERE
+ * @author Robert C. Duvall, Calvin Chen
  */
 public class Main extends Application {
+
     // useful names for constant values used
-    public static final String TITLE = "Example JavaFX Animation";
-    public static final Color DUKE_BLUE = new Color(0, 0.188, 0.529, 1);
-    public static final int SIZE = 400;
+    public static final String TITLE = "Basketball Bouncer";
+    public static final Color DUKE_BLUE = new Color(0, 0.325, 0.608, 1);
+    public static final int SCENE_WIDTH = 1100;
+    public static final int SCENE_HEIGHT = 850;
+    public static final int BLOCK_WIDTH = 50;
+    public static final int BLOCK_HEIGHT = 20;
 
 
     /**
      * Initialize what will be displayed.
      */
     @Override
-    public void start (Stage stage) {
+    public void start(Stage stage) {
         Circle shape = new Circle(200, 200, 40);
         shape.setFill(Color.LIGHTSTEELBLUE);
 
+        Rectangle block = new Rectangle(500, 500, BLOCK_WIDTH, BLOCK_HEIGHT);
+        block.setFill(Color.LIGHTSTEELBLUE);
+
+        Paddle paddle = new Paddle(SCENE_WIDTH, SCENE_HEIGHT);
+
         Group root = new Group();
         root.getChildren().add(shape);
+        root.getChildren().add(block);
+        root.getChildren().add(paddle.getShape());
 
-        Scene scene = new Scene(root, SIZE, SIZE, DUKE_BLUE);
+        Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT, DUKE_BLUE);
         stage.setScene(scene);
+        // respond to input
+        scene.setOnKeyPressed(e -> handleKeyInput(e.getCode(), paddle));
+
 
         stage.setTitle(TITLE);
         stage.show();
+    }
+
+    // Adapted from Robert C. Duvall's "bouncer" code
+    private void handleKeyInput(KeyCode code, Paddle paddle) {
+        if (code == KeyCode.RIGHT) {
+            paddle.moveRight(SCENE_WIDTH);
+        } else if (code == KeyCode.LEFT) {
+            paddle.moveLeft();
+        }
     }
 }
--- src/main/java/breakout/Paddle.java
+++ src/main/java/breakout/Paddle.java
@@ -5,14 +5,14 @@ import javafx.scene.shape.Rectangle;
 public class Paddle {
   public static final int PADDLE_WIDTH = 80;
   public static final int PADDLE_HEIGHT = 10;
-  public static final int PADDLE_SPEED = 20;
+  public static final int PADDLE_SPEED = 12;
 
   // start position of the paddle for each round
-  private int start_x ;
-  private int start_y;
+  private final int start_x;  // middle of scene (horizontally)
+  private final int start_y;  // near bottom (vertically)
 
   // shape of paddle is a Rectangle
-  private Rectangle myShape;
+  private final Rectangle myShape;
 
   /**
    * Create the paddle for a given scene based on scene dimensions
@@ -46,17 +46,61 @@ public class Paddle {
   }
 
   /**
-   * Move paddle left or right based on given directional value (1 for right, -1 for left).
+   * Move paddle left if not contacting left screen edge (else, do not move).
    * elapsedTime is used to ensure consistent speed across different machines. (Robert C. Duvall)
    */
-  public void move (int direction, double elapsedTime) {
-    myShape.setX(myShape.getX() + direction*PADDLE_SPEED*elapsedTime);
+  public void moveLeft () {
+    if (! isContactingLeftEdge())
+      myShape.setX(myShape.getX() + -1*PADDLE_SPEED);
+    else
+      myShape.setX(0);
+  }
+
+  /**
+   * Move paddle right if not contacting right screen edge (else, do not move).
+   * elapsedTime is used to ensure consistent speed across different machines. (Robert C. Duvall)
+   */
+  public void moveRight (int sceneWidth) {
+    if (! isContactingRightEdge(sceneWidth))
+      myShape.setX(myShape.getX() + PADDLE_SPEED);
+    else
+      // left corner of paddle is PADDLE_WIDTH distance from right scene edge
+      myShape.setX(sceneWidth - PADDLE_WIDTH);
+  }
+
+  /**
+   * Returns the distance between the left side of the paddle and the left scene edge.
+   */
+  private double distanceToLeftEdge() {
+    return myShape.getX();
+  }
+
+  /**
+   * Returns the distance between the right side of the paddle and the right scene edge.
+   */
+  private double distanceToRightEdge(int sceneWidth) {
+    return sceneWidth - (myShape.getX() + PADDLE_WIDTH);
+  }
+
+  /**
+   * Returns whether the paddle is contacting the left edge of the scene.
+   */
+  private boolean isContactingLeftEdge() {
+    return (distanceToLeftEdge() <= 0);
+  }
+
+  /**
+   * Returns whether the paddle is contacting the right edge of the scene.
+   */
+  private boolean isContactingRightEdge(int sceneWidth) {
+    return (distanceToRightEdge(sceneWidth) <= 0);
   }
 
   /**
    * Reset paddle to its start position.
    */
   public void reset () {
-    myShape.setX(0);
+    myShape.setX(start_x);
+    myShape.setY(start_y);
   }
 }

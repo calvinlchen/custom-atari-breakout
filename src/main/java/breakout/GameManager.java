e2fbc8aa4318a0dc79d348d0ed7444842939a03c
Calvin Chen <clc162@duke.edu>
2025-01-21 17:02:06 -0500

    Feature BREAK-11: Add methods for displaying score and incrementing score when ball hits block

--- src/main/java/breakout/GameManager.java
+++ src/main/java/breakout/GameManager.java
@@ -10,6 +10,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -17,12 +19,15 @@ public class GameManager {
   private int myLivesRemaining;
   private int myCurrentLevel;
   private int myScore;
+  private int myHighScore;
 
   private Group mySceneRoot;
   private Stage myStage;
   private Paddle myPaddle;
   private List<Ball> myBalls;
   private List<Block> myBlocks;
+  private Text myScoreText;
+  private Text myLivesText;
 
   private int mySceneWidth;
   private int mySceneHeight;
@@ -34,14 +39,20 @@ public class GameManager {
   public static final int FRAMES_PER_SECOND = 60;
   public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
 
+  public static final int SCORE_INCREMENT = 100;
+
   public GameManager(int sceneWidth, int sceneHeight, Stage stage) {
     myLivesRemaining = 3;
     myCurrentLevel = 0;
     myScore = 0;
+    myHighScore = 0;
+
+    myScoreText = createScoreText();
 
     mySceneWidth = sceneWidth;
     mySceneHeight = sceneHeight;
     myStage = stage;
+
   }
 
   public void startLevel(int levelNumber) {
@@ -56,7 +67,7 @@ public class GameManager {
    * @param levelName level number in "lvl_xx" format (i.e. lvl_01)
    * @return root of all scene objects
    */
-  public Group createSceneRoot(String levelName) {
+  private Group createSceneRoot(String levelName) {
     // Create Block objects from file, store in LevelMap
     LevelMap levelMap = new LevelMap(MAPS_FILE_PREFIX+levelName+".txt",
         mySceneWidth, mySceneHeight);
@@ -78,6 +89,8 @@ public class GameManager {
       root.getChildren().add(block.getShape());
     }
 
+    root.getChildren().add(myScoreText);
+
     return root;
   }
 
@@ -85,7 +98,7 @@ public class GameManager {
    * Initialize what will be displayed.
    * Adapted from code by Robert C. Duvall.
    */
-  public void initializeScene() {
+  private void initializeScene() {
     Scene scene = new Scene(mySceneRoot, mySceneWidth, mySceneHeight, Color.BLACK);
     myStage.setScene(scene);
     myStage.setTitle(Main.TITLE);
@@ -107,36 +120,76 @@ public class GameManager {
    */
   private void step (double elapsedTime) {
     for (Ball ball : myBalls) {
-      // Check and bounce off wall and paddle if necessary
-      ball.bounceOffEdge(mySceneWidth);
-      ball.bounceOffPaddle(myPaddle);
+      // Check and bounce off wall/ceiling if necessary
+      ball.checkAndBounceEdge(mySceneWidth);
 
-      for (Block block : myBlocks) {
-        if (block.checkIfShouldBounce(ball)) {
-          block.bounceBall(ball);
-          myScore += 100;
-          block.hitBlockActions();
-          // skips rest of the loop after the first block bounce
-          break;
-        }
-      }
+      // Check and bounce the ball off paddle if necessary
+      myPaddle.checkAndBounceBall(ball);
+
+      // Check and bounce the ball off a block if contacting
+      checkBlocksAndBounceBall(ball);
 
       ball.move(elapsedTime);
 
       if (ball.isContactingFloor(mySceneHeight)) {
-        ball.stopMotion();
+        whenBallHitsFloor(ball);
       }
     }
   }
 
   /**
-   * Lists actions taken if ball hits the bottom of the map.
+   * Check all blocks and bounce the ball if it is contacting a block.
+   * @param ball Ball object to bounce if contacting a block
+   */
+  private void checkBlocksAndBounceBall(Ball ball) {
+    for (Block block : myBlocks) {
+      if (block.checkIfShouldBounce(ball)) {
+        block.bounceBall(ball);
+        incrementScore();
+        block.hitBlockActions();
+        // skips rest of the loop after the first block bounce
+        break;
+      }
+    }
+  }
+
+  /**
+   * Actions taken if ball hits the bottom of the map.
    */
   private void whenBallHitsFloor(Ball ball) {
     myLivesRemaining--;
-    if (myLivesRemaining != 0) {
+    if (myLivesRemaining > 0) {
       ball.reset();
     }
+    else {
+      ball.stopMotion();
+      whenPlayerLoses();
+    }
+  }
+
+  private void incrementScore() {
+    myScore += SCORE_INCREMENT;
+    myScoreText.setText(getScoreTextString());
+  }
+
+  /**
+   * Actions taken when a player loses the game (has no lives remaining)
+   */
+  private void whenPlayerLoses() {
+
+  }
+
+  private Text createScoreText() {
+    Text scoreText = new Text(getScoreTextString());
+    scoreText.setFill(Color.WHITE);
+    scoreText.setX(Main.SCENE_WIDTH * 0.1);
+    scoreText.setY(Main.SCENE_HEIGHT * 0.95);
+    scoreText.setFont(new Font(Main.BODY_FONT_SIZE));
+    return scoreText;
+  }
+
+  private String getScoreTextString() {
+    return "Score: " + myScore;
   }
 
   /**
@@ -150,10 +203,16 @@ public class GameManager {
       myPaddle.moveLeft();
     }
     if (code == KeyCode.SPACE) {
-      for(Ball ball : myBalls) {
-        if (!ball.isMoving()) {
-          ball.startMotion();
-        }
+      if (myLivesRemaining > 0) {
+        setAllBallsInMotion();
+      }
+    }
+  }
+
+  private void setAllBallsInMotion() {
+    for(Ball ball : myBalls) {
+      if (!ball.isMoving()) {
+        ball.startMotion();
       }
     }
   }
